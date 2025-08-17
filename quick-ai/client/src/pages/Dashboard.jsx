2db9bc68032f0478240fbdbf02b6c0fb3e96c85d
Zeet7450 <verrel.gunawan555@gmail.com>
2025-08-17 22:52:21 +0700

    duplicate done

--- quick-ai/client/src/pages/Dashboard.jsx
+++ quick-ai/client/src/pages/Dashboard.jsx
@@ -3,13 +3,31 @@ import { dummyCreationData } from '../assets/assets'
 import { Gem, Sparkles } from 'lucide-react'
 import { Protect } from '@clerk/clerk-react'
 import CreationItem from '../components/CreationItem'
+import axios from 'axios'
+import toast from 'react-hot-toast'
+import { useAuth } from '@clerk/clerk-react'
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
 
   const [creations, setCreations] = useState([])
+  const [loading, setLoading] = useState(true)
+  const { getToken } = useAuth()
 
   const getDashboardData = async () => {
-    setCreations(dummyCreationData)
+    try {
+      const { data } = await axios.get('/api/user/get-user-creations', { headers: { Authorization: `Bearer ${await getToken()}` } })
+
+      if (data.success) {
+        setCreations(data.creations);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -27,7 +45,7 @@ const Dashboard = () => {
             <h2 className='text-xl font-semibold'>{creations.length}</h2>
           </div>
           <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
-            <Sparkles className='w-5 text-white'/>
+            <Sparkles className='w-5 text-white' />
           </div>
         </div>
 
@@ -40,23 +58,30 @@ const Dashboard = () => {
               <Protect plan='Premium' fallback='Free'></Protect></h2>
           </div>
           <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
-            <Gem className='w-5 text-white'/>
+            <Gem className='w-5 text-white' />
           </div>
         </div>
 
 
       </div>
 
-    <div className='space-y-3'>
-      <p className='mt-6 mb-4'>Recent Creations</p>
-      {
-        creations.map((item) => <CreationItem key={item.id} item={item} />)
-      }
-    </div>
+      {loading ? (
+        <div className='flex justify-center items-center h-3/4'>
+          <div className='animate-spin rounded-full h-11 w-11 border-3 border-purple-500 border-t-transparent'></div>
+        </div>
+      ) : (
+        <div className='space-y-3'>
+          <p className='mt-6 mb-4'>Recent Creations</p>
+          {creations.map((item) => <CreationItem key={item.id} item={item} />)}
+        </div>
+      )}
 
 
     </div>
   )
+
+
+
 }
 
 export default Dashboard
